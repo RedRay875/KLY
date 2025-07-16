493297a08defd81ece9d5eb294e01196abc2b648
RedRay875 <priyanshuray1030@gmail.com>
2025-07-16 22:39:16 +0530

    Delete Climate checker directory

--- Climate checker/src/apiKeys.js
+++
@@ -1,5 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "ce0561de1dd991660f6b45d4850ce57e",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
